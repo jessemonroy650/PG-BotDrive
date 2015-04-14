bc67f2bb0a4ad4f49cd9707a8106d532fbb92c36
Jesse Monroy <jessemonroy650@yahoo.com>
2015-04-13 23:14:44 -0700

    update layout, add menu icon, change pg-networkinfo.js

--- pg-network-info.js
+++ pg-network-info.js
@@ -1,32 +1,43 @@
-    function onOffline() {
+/*
+	Date: 2015-04-13
+*/
+var pgNetworkInfo = {
+	//
+    var states = {
+		Connection.UNKNOWN : 'Unknown',
+	    Connection.ETHERNET : 'Ethernet';
+    	Connection.WIFI     : 'WiFi',
+	    Connection.CELL_2G : 'Cell 2G',
+	    Connection.CELL_3G : 'Cell 3G',
+    	Connection.CELL_4G :'Cell 4G',
+	    Connection.CELL    : 'Cell generic',
+	    Connection.NONE    : 'No network'
+	},
+   	var networkState = this.states[Connection.UNKNOWN];
+	//
+	onOffline : function() {
 		thePlace = document.getElementById('ns');
+		// flip the button color
 		thePlace.classList.remove('button-action');
 		thePlace.classList.add('button-caution');
+		// Change the text to reflect the state
 		thePlace = document.getElementById('networkstatus').innerHTML = 'Offline';
 		updateConnection();
-    }
+    },
 	//
-    function onOnline() {
+	onOnline : function() {
 		thePlace = document.getElementById('ns');
+		// flip the button color
 		thePlace.classList.remove('button-caution');
 		thePlace.classList.add('button-action');
+		// Change the text to reflect the state
 		thePlace = document.getElementById('networkstatus').innerHTML = 'Online';
 		updateConnection();
-    }
+    },
 	//
-	function updateConnection() {
-    	var networkState = navigator.connection.type;
-
-	    var states = {};
-    	states[Connection.UNKNOWN]  = 'Unknown connection';
-	    states[Connection.ETHERNET] = 'Ethernet connection';
-    	states[Connection.WIFI]     = 'WiFi connection';
-	    states[Connection.CELL_2G]  = 'Cell 2G connection';
-	    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    	states[Connection.CELL_4G]  = 'Cell 4G connection';
-	    states[Connection.CELL]     = 'Cell generic connection';
-	    states[Connection.NONE]     = 'No network connection';
-
-		thePlace = document.getElementById('networktype').innerHTML = states[networkState];
-		//alert('Connection type: ' + states[networkState]);
+	updateConnection : function() {
+    	networkState = navigator.connection.type;
+		thePlace = document.getElementById('networktype').innerHTML = this.states[networkState];
 	}
+
+}
