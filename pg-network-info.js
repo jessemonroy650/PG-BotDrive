3b95e103aef76d256a6e23e167af45880c26229b
Jesse Monroy <jessemonroy650@yahoo.com>
2015-04-13 23:55:47 -0700

    back to original methods.

--- pg-network-info.js
+++ pg-network-info.js
@@ -1,47 +1,40 @@
 /*
 	Date: 2015-04-13
 */
-var pgNetworkInfo = {
 	//
-	var states = {},
-	//
-	init : function () {
-	this.states : { 
-		Connection.UNKNOWN : "Unknown",
-	    Connection.ETHERNET : "Ethernet",
-    	Connection.WIFI     : "WiFi",
-	    Connection.CELL_2G : "Cell 2G",
-	    Connection.CELL_3G : "Cell 3G",
-    	Connection.CELL_4G : "Cell 4G",
-	    Connection.CELL    : "Cell generic",
-	    Connection.NONE    : "No network"
-	}
-	},
-   	//networkState = this.states[Connection.UNKNOWN],
-	//
-	onOffline : function() {
+	function onOffline() {
 		thePlace = document.getElementById('ns');
 		// flip the button color
 		thePlace.classList.remove('button-action');
 		thePlace.classList.add('button-caution');
 		// Change the text to reflect the state
-		thePlace = document.getElementById('networkstatus').innerHTML = 'Offline';
-		this.updateConnection();
-    },
+		document.getElementById('networkstatus').innerHTML = 'Offline';
+		updateConnection();
+    };
 	//
-	onOnline : function() {
+	function onOnline() {
 		thePlace = document.getElementById('ns');
 		// flip the button color
 		thePlace.classList.remove('button-caution');
 		thePlace.classList.add('button-action');
 		// Change the text to reflect the state
-		thePlace = document.getElementById('networkstatus').innerHTML = 'Online';
-		this.updateConnection();
-    },
+		document.getElementById('networkstatus').innerHTML = 'Online';
+		updateConnection();
+    };
 	//
-	updateConnection : function() {
+	function updateConnection() {
     	var networkState = navigator.connection.type;
-		thePlace = document.getElementById('networktype').innerHTML = this.states[networkState];
-	}
+		var states = {};
+		states[Connection.UNKNOWN] = "Unknown";
+	    states[Connection.ETHERNET] = "Ethernet";
+    	states[Connection.WIFI] = "WiFi";
+	    states[Connection.CELL_2G] = "Cell 2G";
+	    states[Connection.CELL_3G] = "Cell 3G";
+    	states[Connection.CELL_4G] = "Cell 4G";
+	    states[Connection.CELL] = "Cell generic";
+	    states[Connection.NONE] = "No network";
 
-}
+		document.getElementById('networktype').innerHTML = states[networkState];
+	};
+
+
