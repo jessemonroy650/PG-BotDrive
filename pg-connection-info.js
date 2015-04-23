dec0d51d37ae1f83b7356ee76cc73fbb1a977f83
Jesse Monroy <jessemonroy650@yahoo.com>
2015-04-22 23:14:19 -0700

    renamed pg-network-info.js to pg-connecti

--- pg-connection-info.js
+++ pg-connection-info.js
@@ -1,34 +1,37 @@
 /*
 	Date: 2015-04-13
 */
+var connectionInfo = {
+	networkState : null,
+	states : {},
 	//
-	function onOffline() {
+	offline : function () {
 		thePlace = document.getElementById('nt');
 		// flip the button color
 		thePlace.classList.remove('button-action');
 		thePlace.classList.add('button-caution');
-		updateConnection();
-    };
+		connectionInfo.updateConnection();
+    },
 	//
-	function onOnline() {
+	online : function () {
 		thePlace = document.getElementById('nt');
-		// flip the button color
 		thePlace.classList.remove('button-caution');
 		thePlace.classList.add('button-action');
-		updateConnection();
-    };
+		connectionInfo.updateConnection();
+    },
 	//
-	function updateConnection() {
-    	var networkState = navigator.connection.type;
-		var states = {};
-		states[Connection.UNKNOWN] = "Unknown";
+	updateConnection : function () {
+		networkState = navigator.connection.type;
+		//var states = {};
+		states[Connection.UNKNOWN]  = "Unknown";
 	    states[Connection.ETHERNET] = "Ethernet";
-    	states[Connection.WIFI] = "WiFi";
-	    states[Connection.CELL_2G] = "Cell 2G";
-	    states[Connection.CELL_3G] = "Cell 3G";
-    	states[Connection.CELL_4G] = "Cell 4G";
+    	states[Connection.WIFI]     = "WiFi";
+	    states[Connection.CELL_2G]  = "Cell 2G";
+	    states[Connection.CELL_3G]  = "Cell 3G";
+    	states[Connection.CELL_4G]  = "Cell 4G";
 	    states[Connection.CELL] = "Cell generic";
 	    states[Connection.NONE] = "No network";
 
 		document.getElementById('networktype').innerHTML = states[networkState];
-	};
+	}
+};
